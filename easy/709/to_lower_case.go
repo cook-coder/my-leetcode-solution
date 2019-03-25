59134272b51d61e2c3b365e58902006249cd33a7
cook-coder <zhangrcheng@gmail.com>
2019-03-25 22:26:08 +0800

    modified

--- easy/709/to_lower_case.go
+++ easy/709/to_lower_case.go
@@ -1,8 +1,11 @@
 package toLowerCase
 
+import "fmt"
+
 func toLowerCase(str string) string {
 	strRunes := []rune(str)
 	for i, c := range strRunes {
+		fmt.Println(i, c)
 		if c > 64 && c < 91 {
 			strRunes[i] += 32
 		}
--- easy/709/to_lower_case_test.go
+++ easy/709/to_lower_case_test.go
@@ -1,10 +1,15 @@
 package toLowerCase
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestToLowerCase(t *testing.T) {
 	str := "Hello Wolrd!"
 	str = "here"
 	str = "LOVELY"
+	str = "😁😆Hello！ État壹"
 	t.Log(toLowerCase(str))
+	t.Log(strings.ToLower(str))
 }
